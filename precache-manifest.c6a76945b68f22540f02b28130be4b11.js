6aa15649629ba13440c4002f37b7db3d33434255
Ricardo Leme <ricardo.leme@gmail.com>
2020-10-16 13:41:54 -0300

    Updates

---
+++ precache-manifest.c6a76945b68f22540f02b28130be4b11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa362dee894eb350eaefeea74d476c8",
+    "url": "/siteReact/index.html"
+  },
+  {
+    "revision": "e50037c8647e663cd447",
+    "url": "/siteReact/static/js/2.0c7da648.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/siteReact/static/js/2.0c7da648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842c9eb279be35fdf627",
+    "url": "/siteReact/static/js/main.aaa83559.chunk.js"
+  },
+  {
+    "revision": "601b15347c0f7683534a",
+    "url": "/siteReact/static/js/runtime-main.cc7b0e4e.js"
+  }
+]);
